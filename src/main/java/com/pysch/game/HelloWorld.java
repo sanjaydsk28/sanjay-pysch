587244b2fcc23cd5972eb7c60e1fe2a454e50ec7
Sanjay Kumar <sanjaykumar.dara123@gmail.com>
2020-02-21 18:55:22 +0530

    HelloWorld

---
+++ src/main/java/com/pysch/game/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.pysch.game;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dev-test")
+public class HelloWorld {
+    @GetMapping("/")
+    public  String Hello() {
+        return "Welcome to Pysch World!!";
+    }
+}
